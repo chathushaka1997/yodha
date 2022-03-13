2f1669887b28fffa579343cdfbc4c776ce9e7d0a
lochana <chathushaka97@gmail.com>
2022-03-14 00:42:45 +0530

    dsf

--- src/components/guest/homepage/ContentComp.jsx
+++ src/components/guest/homepage/ContentComp.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import img1 from "../../../assets/images/yodha-wewa.jpg";
+import img2 from "../../../assets/images/wehera.jpg";
+import img3 from "../../../assets/images/tissawewa.jpg";
 import room1 from "../../../assets/images/room.png";
+import room2 from "../../../assets/images/room1.png";
+import room3 from "../../../assets/images/room2.png";
 import food1 from "../../../assets/images/food.png";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+
+// import required modules
+import { Pagination,Navigation } from "swiper";
 
 const ContentComp = () => {
   return (
     <div className="container contentComp">
       <div className="row overflow-hidden">
-        <div className="col-6">
-          <h6>A little about us</h6>
+        <div className="col-sm-12 col-md-12 col-lg-6 ">
+          <h6 style={{ color: "#707070" }}>A little about us</h6>
           <h1 className="main-title mb-5">Recreation and recovery in healing waters</h1>
+          <img className="mb-5  d-sm-block d-md-block d-lg-none" src={img1} alt="yodha wewa, yodha resort" style={{ width: "100%" }} />
           <p>
             Yodha resort is a piece of paradise among the desert areas of Sri Lanka. Locals often call this territory an Oasis and dream of
             swimming in a luxurious pool. What can we say about the local mineral water, which has already cured more than one thousand
@@ -24,35 +39,158 @@ const ContentComp = () => {
             strength and prolong youth.
           </p>
         </div>
-        <div className="col-6 overflow-hidden">
-          <img className="ms-5 ps-3 mt-5 pt-3" src={img1} alt="yodha wewa, yodha resort" style={{width:"90%"}} />
+        <div className="d-none d-lg-block col-sm-0 col-md-0 col-lg-6 overflow-hidden ">
+          <img
+            className="ms-5 ps-3 mt-5 pt-3 d-sm-none d-md-none d-lg-block"
+            src={img1}
+            alt="yodha wewa, yodha resort"
+            style={{ width: "90%" }}
+          />
         </div>
       </div>
 
       <div className="row mt-3 overflow-hidden">
-        <h1 className="main-title mt-5 mb-5">What do we offer</h1>
+        <h1 className="main-title mt-xs-3 mt-sm-3 mt-md-5 mb-5">What do we offer</h1>
         <div className="d-flex justify-content-between">
-        <Card style={{ width: "23rem" }} >
-          <Card.Img variant="top" src={room1} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "23rem" }}>
-          <Card.Img variant="top" src={food1} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "23rem" }}>
-          <Card.Img variant="top" src={room1} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-          </Card.Body>
-        </Card>
+          <div className="row mb-2">
+            <div className="col-sm-12 col-md-4">
+              <Card className="m-3">
+                <Card.Img variant="top" src={room1} />
+                <Card.Body>
+                  <Card.Title className="main-title2">Comfortable rooms</Card.Title>
+                  <Card.Text>
+                    Your comfort is our concern <br />
+                    Fresh renovation <br />
+                    Helpful staff
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-sm-12 col-md-4">
+              <Card className="m-3">
+                <Card.Img variant="top" src={food1} />
+                <Card.Body>
+                  <Card.Title className="main-title2">Lankan cuisine</Card.Title>
+                  <Card.Text>
+                    Circular shower <br />
+                    Fresh drinking water <br />
+                    Health for the whole family
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-sm-12 col-md-4">
+              <Card className="m-3">
+                <Card.Img variant="top" src={room1} />
+                <Card.Body>
+                  <Card.Title className="main-title2">Comfortable rooms</Card.Title>
+                  <Card.Text>
+                    Your comfort is our concern <br />
+                    Fresh renovation <br />
+                    Helpful staff
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-12 col-lg-7">
+            <h1 className="main-title">Entertainment and attractions</h1>
+            <div className="row mt-5 mt-md-5">
+              <div className="col-sm-12 col-md-6 ">
+                <img src={img3} alt="" style={{ width: "100%" }} />
+              </div>
+              <div className="col-sm-12 col-md-6">
+                <p className="mt-1 mt-sm-3">
+                  You can use excursion services. Visit places that have already become historical - the tissa wewa, the temple, the stupa.
+                  The combination of active recreation and relaxation will help restore strength. The new work week will start from scratch.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-12 col-lg-5 overflow-hidden mt-1 mt-md-4">
+            <img src={img2} alt="" style={{ width: "60%" }} />
+          </div>
+        </div>
+      </div>
+      <div className="container mt-5">
+        <div className="d-flex" style={{ alignItems: "baseline" }}>
+          <h1 className="main-title me-3">Rooms</h1>
+          <hr className="flex-grow" style={{ color: "#E1963C", padding: "2px", flex: "1" }} />
+        </div>
+        <div className="row mt-3 mt-md-5">
+          <div className="col-sm-12 col-md-4 overflow-hidden">
+            <img src={room1} alt="" style={{ height: "300px", width: "100%", objectFit: "cover" }} />
+          </div>
+          <div className="col-sm-12 col-md-4 ms-0 ms-md-5">
+            <h1 className="main-title3 mb-4">Standard</h1>
+            <h6 className="mb-4">
+              From <b style={{ fontSize: "25px" }}>18 000</b> /day.
+            </h6>
+            <p>
+              The area of ​​the room is 20 sq.m. <br /> Balcony 3 sq.m <br /> Shower and all accessories 2 twin beds
+            </p>
+            <h6 className="mb-5 mb-md-0">LEARN MORE &#10142;</h6>
+          </div>
+        </div>
+        <div className="row mt-3 mt-md-5">
+          <div className="col-sm-12 col-md-4 overflow-hidden">
+            <img src={room2} alt="" style={{ height: "300px", width: "100%", objectFit: "cover" }} />
+          </div>
+          <div className="col-sm-12 col-md-4 ms-0 ms-md-5">
+            <h1 className="main-title3 mb-4">The luxury</h1>
+            <h6 className="mb-4">
+              From <b style={{ fontSize: "25px" }}>18 000</b> /day.
+            </h6>
+            <p>
+              The square of the room is 20 sq.m. <br /> Balcony 3 sq.m. <br /> Shower and all accessories for 2 persons (double bed)
+            </p>
+            <h6 className="mb-5 mb-md-0">LEARN MORE &#10142;</h6>
+          </div>
+        </div>
+        <div className="row mt-3 mt-md-5">
+          <div className="col-sm-12 col-md-4 overflow-hidden">
+            <img src={room3} alt="" style={{ height: "300px", width: "100%", objectFit: "cover" }} />
+          </div>
+          <div className="col-sm-12 col-md-4 ms-0 ms-md-5">
+            <h1 className="main-title3 mb-4">Suite</h1>
+            <h6 className="mb-4">
+              From <b style={{ fontSize: "25px" }}>18 000</b> /day.
+            </h6>
+            <p>
+              The area of ​​the room is 20 sq.m. <br /> Balcony 3 sq.m <br /> Shower and all accessories 2 twin beds
+            </p>
+            <h6 className="mb-5 mb-md-0">LEARN MORE &#10142;</h6>
+          </div>
+        </div>
+      </div>
+      <div className="container mt-3 mt-md-5">
+        <div className="d-flex" style={{ alignItems: "center" }}>
+          <hr className="flex-grow" style={{ color: "#E1963C", padding: "2px", flex: "0.2" }} />
+          <h1 className="main-title ms-3">Gallery</h1>
+        </div>
+        <div className="mt-4">
+        <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination,Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><div className="overflow-hidden"><img src={food1} alt="" style={{height:"350px",width:"100%",objectFit:"cover"}}/></div></SwiperSlide>
+        <SwiperSlide><div className="overflow-hidden"><img src={room1} alt="" style={{height:"350px",width:"100%",objectFit:"cover"}}/></div></SwiperSlide>
+        <SwiperSlide><div className="overflow-hidden"><img src={room2} alt="" style={{height:"350px",width:"100%",objectFit:"cover"}}/></div></SwiperSlide>
+        <SwiperSlide><div className="overflow-hidden"><img src={room3} alt="" style={{height:"350px",width:"100%",objectFit:"cover"}}/></div></SwiperSlide>
+        <SwiperSlide><div className="overflow-hidden"><img src={img1} alt="" style={{height:"350px",width:"100%",objectFit:"cover"}}/></div></SwiperSlide>
+        <SwiperSlide><div className="overflow-hidden"><img src={img2} alt="" style={{height:"350px",width:"100%",objectFit:"cover"}}/></div></SwiperSlide>
+        <SwiperSlide><div className="overflow-hidden"><img src={img3} alt="" style={{height:"350px",width:"100%",objectFit:"cover"}}/></div></SwiperSlide>
+        
+      </Swiper>
         </div>
       </div>
     </div>
--- src/components/guest/homepage/FirstBannerComp.jsx
+++ src/components/guest/homepage/FirstBannerComp.jsx
@@ -6,16 +6,32 @@ const FirstBannerComp = () => {
   return (
     <div className="bannerContainer">
       <NavbarComponent></NavbarComponent>
-      {/*  <img src={bgImg} alt="yodha resort" /> */}
-     
-      <input type="date" className="form-control" placeholder="Chack In : &nbsp; "/>
-      <input type="date" className="form-control" placeholder="Check out : &nbsp; " />
-      <Form.Select aria-label="Default select example">
-        <option>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-      </Form.Select>
+      <div className="container checkin-container">
+        <div>
+          <h1 className="main-title1 mb-5">Enjoy the gentle breeze on the Yodha lake</h1>
+          <div className="row">
+            <div className="col-sm-12 col-md-12 col-lg-3">
+              <input type="date" className="form-control me-5 mb-4" placeholder="Check In : &nbsp; " />
+            </div>
+            <div className="col-sm-12 col-md-12 col-lg-3">
+              <input type="date" className="form-control me-5 mb-4" placeholder="Check out : &nbsp; " />
+            </div>
+            <div className="col-sm-12 col-md-12 col-lg-3">
+              <Form.Select aria-label="Default select example" style={{  marginRight: "100px" }} className="mb-4">
+                <option>Persons</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </div>
+            <div className="col-sm-12 col-md-12 col-lg-3">
+              <button className="btn ps-5 pe-5" style={{ background: "#E1963C",width:"100%"  }}>
+                Book
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
